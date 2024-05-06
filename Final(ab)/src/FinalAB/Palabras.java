1e03cf325b4757afc344cef9e79a9321c5e79f09
Sebastián Rodríguez Torres <104276355+SeRaTo12@users.noreply.github.com>
2024-05-06 17:45:28 -0600

    Version Final
    Modificados las validaciones y sustituidas para que muestre el procedimiento de una maquina de turing

--- Final(ab)/src/FinalAB/Palabras.java
+++ Final(ab)/src/FinalAB/Palabras.java
@@ -4,6 +4,9 @@
  */
 package FinalAB;
 
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -20,44 +23,80 @@ public class Palabras {
     
     
     public static void nueva(){
-        boolean aprobado = true;
-        palabra="";
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingresa la palabra (solo Minusculas): ");
-        palabra = entrada.nextLine();
-        if(palabra.length()==0){
-            aprobado = false;
-        }
-        Palabras.add(palabra);
-        for (int i = 0; i < palabra.length(); i++) {
-            if(palabra.length()%2!=0 || palabra.isEmpty()){
+        try {
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out, "UTF-8"));
+            writer.flush();
+            
+            boolean aprobado = true;
+            palabra="";
+            Scanner entrada = new Scanner(System.in);
+            System.out.println("Ingresa la palabra (solo Minusculas): ");
+            palabra = entrada.nextLine();
+            String palabra2 = "";
+            if(palabra.length()==0){
                 aprobado = false;
-                break;
             }
-            if(i%2==0){
-                if(palabra.charAt(i)==97){
-                    continue;
-                }else{
+            Palabras.add(palabra);
+            for (int i = 0; i < palabra.length(); i++) {
+                if(palabra.length()%2!=0 || palabra.isEmpty()){
                     aprobado = false;
-                    break;
                 }
-            }else{
-                if(palabra.charAt(i)==98){
-                    continue;
+                if(i%2==0){
+                    if(palabra.charAt(i)==97){
+                        palabra2 +="µ";
+                        if(i==0){
+                            writer.println("(i,a)=(q1,µ,R) -> "+palabra2+"\n");
+                            writer.flush();
+                        }else{
+                            writer.println("(qi,a)=(q1,µ,R) -> "+palabra2+"\n");
+                            writer.flush();
+                        }
+                    }else{
+                        aprobado = false;
+                        palabra2+="_";
+                        if(i==0){
+                            writer.println("(i,a)=(q1,µ,R) -> "+palabra2+"\n");
+                            writer.flush();
+                        }else{
+                            writer.println("(q1,a)=(q1,µ,R) -> "+palabra2+"\n");
+                            writer.flush();
+                        }
+                        break;
+                    }
+                    
                 }else{
-                    aprobado = false;
-                    break;
+                    if(palabra.charAt(i)==98){
+                        palabra2 +="ß";
+                        writer.println("(q1,b)=(q1,ß,R) -> "+palabra2+"\n");
+                        writer.flush();
+                    }else{
+                        aprobado = false;
+                        palabra2+="_";
+                        writer.println("(b,R) -> "+palabra2+"\n");
+                        writer.flush();
+                        break;
+                    }
                 }
             }
-        }
+            
+            if(aprobado){
 
-        if(aprobado){
-            System.out.println("palabra valida");
-            PalabrasValidas.add(palabra);
-        }else{
-            System.out.println("Palabra no valida");
-            PalabrasNoValidas.add(palabra);
+                writer.println("(q2,■)=(T,■,R)-> "+palabra2);
+                System.out.println("palabra valida");
+                writer.println("Palabra ingresada: "+ palabra+"\nMaquina de turing: "+palabra2);
+                writer.flush();
+                PalabrasValidas.add(palabra);
+            }else{
+                System.out.println("Palabra no valida");
+                writer.println("Palabra ingresada: "+ palabra+"\nMaquina de turing: "+palabra2);
+                writer.flush();
+                PalabrasNoValidas.add(palabra);            
+            }
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("Error: Codificación no soportada.");
         }
+        
+        
     }
     
     public static void revision(){
